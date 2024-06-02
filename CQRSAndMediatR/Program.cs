d3b94b11fe5fc9a2752fb28e1e4a15f5a1370d55
fater barhoum <faterbarhoum@outlook.com>
2024-06-02 05:30:16 +0300

    Add Minimal API Endpoints

--- CQRSAndMediatR/Program.cs
+++ CQRSAndMediatR/Program.cs
@@ -1,4 +1,9 @@
+using CQRSAndMediatR.Features.Products.Commands.Create;
+using CQRSAndMediatR.Features.Products.Commands.Delete;
+using CQRSAndMediatR.Features.Products.Queries.Get;
+using CQRSAndMediatR.Features.Products.Queries.List;
 using CQRSAndMediatR.Persistence;
+using MediatR;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -28,5 +33,35 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+#region Minimal API Endpoints
+app.MapGet("/products/{id:guid}", async (Guid id, ISender mediatr) =>
+{
+	var product = await mediatr.Send(new GetProductQuery(id));
+	if (product == null) return Results.NotFound();
+	return Results.Ok(product);
+});
+
+app.MapGet("/products", async (ISender mediatr) =>
+{
+	var products = await mediatr.Send(new ListProductQuery());
+	return Results.Ok(products);
+});
+
+app.MapPost("/products", async (CreateProductCommand command, ISender mediatr) =>
+{
+	var productId = await mediatr.Send(command);
+	if (Guid.Empty == productId) return Results.BadRequest();
+	return Results.Created($"/products/{productId}", new { id = productId });
+});
+
+app.MapDelete("/products/{id:guid}", async (Guid id, ISender mediatr) =>
+{
+	await mediatr.Send(new DeleteProductCommand(id));
+	return Results.NoContent();
+});
+
+
+#endregion
 
 app.Run();
