f01fc7d7ccf896504ba6d1c4235bd5d643de44c8
fater barhoum <faterbarhoum@outlook.com>
2024-06-02 04:15:56 +0300

    add DBConte and register it in program

--- CQRSAndMediatR/Program.cs
+++ CQRSAndMediatR/Program.cs
@@ -4,6 +4,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+// register the dbcontect
+builder.Services.AddDbContext<AppDbContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
