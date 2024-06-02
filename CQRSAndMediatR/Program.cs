a465abec5b108a262c8814e563f5d0c8850fe375
fater barhoum <faterbarhoum@outlook.com>
2024-06-02 05:26:26 +0300

    add swagger

--- CQRSAndMediatR/Program.cs
+++ CQRSAndMediatR/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // register the dbcontect
 builder.Services.AddDbContext<AppDbContext>();
@@ -17,6 +19,11 @@ builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.Get
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
 
 app.UseAuthorization();
 
